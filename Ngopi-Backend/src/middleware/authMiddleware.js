265d3ba8c35637597ff99695265178b9f8f6925e
Retiortuk <aauliagazzam@gmail.com>
2025-09-10 12:11:30 +0700

    feat: User Model, authMiddleware(protect, admin)

---
+++ Ngopi-Backend/src/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import asyncHandler from 'express-async-handler';
+
+const protect = asyncHandler(async (req, res, next)=> {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Save Token to Token
+            token = req.headers.authorization.split(' ')[1];
+            // verify the token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            // Take the user data from token wihtout password saved it to req.user
+            req.user = await User.findById(decoded.id).select('-password');
+            // next when it's done
+            next();
+        } catch (error) {
+            console.log(error)
+            res.status(401);
+            throw new Error('Not Authorized, Token Not Valid');
+        }
+    }
+    
+    if(!token) {
+        res.status(401);
+        throw new Error('Not Authorized, No Token');
+    }
+});
+
+const admin = (req, res, next) => {
+    if(req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(403);
+        throw new Error('Not Authorized as an Admin');
+    }
+};
+
+export default {protect, admin};
