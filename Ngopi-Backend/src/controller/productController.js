165fc8cce5e7f474e0799dff9b1b71ba22c1a96d
Retiortuk <aauliagazzam@gmail.com>
2025-09-12 13:31:53 +0700

    Add: UpdateProduct and DeleteProduct on controller

--- Ngopi-Backend/src/controller/productController.js
+++ Ngopi-Backend/src/controller/productController.js
@@ -27,4 +27,37 @@ const postProduct = asynchandler(async(req,res)=> {
     res.status(201).json(createdProduct);
 });
 
-export {getProducts, postProduct};
+// DELETE PRODUCTS
+const deleteProduct = asynchandler(async(req,res)=> {
+    const toDelete = await Product.findById(req.params.id);
+    if(toDelete){
+        await toDelete.deleteOne();
+        res.json({message: 'Produk Berhasil Dihapus'})
+    } else {
+        res.status(404);
+        throw new Error('Produk Tidak Ditemukan');
+    }
+});
+
+// UPDATE Product
+const updateProduct = asynchandler(async(req,res)=> {
+    const product = await Product.findById(req.params.id);
+    
+    if(product){
+        product.name = req.body.name || product.name;
+        product.price = req.body.price || product.price;
+        product.image = req.body.image || product.image
+
+        const updatedProduct = await product.save();
+        res.json({
+            name: updatedProduct.name,
+            price: updatedProduct.price,
+            image: updatedProduct.image,
+        });
+    } else {
+        res.status(404);
+        throw new Error('Produk Tidak Ditemukan');
+    }
+});
+
+export {getProducts, postProduct, deleteProduct, updateProduct};
--- Ngopi-Backend/src/routes/productRoutes.js
+++ Ngopi-Backend/src/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import { admin, protect } from '../middleware/authMiddleware.js';
-import { getProducts, postProduct } from '../controller/productController.js';
+import { deleteProduct, getProducts, postProduct, updateProduct } from '../controller/productController.js';
 
 router.get('/', getProducts);
 router.post('/post', protect, admin, postProduct);
+router.delete('/delete/:id', protect, admin, deleteProduct);
+router.put('/update/:id', protect, admin, updateProduct);
 
 export default router;
