0e04edfaf6e0da5712e62e1dc509733b98cc6dd7
RetiortUK <aauliagazzam@gmail.com>
2025-09-15 22:57:40 +0700

    AuthContext

---
+++ Ngopi-Frontend/src/contexts/AuthContext.jsx
@@ -0,0 +1,75 @@
+import React, {useContext, createContext, useState, useEffect} from "react";
+import apiClient from "../api/axiosConfig.js";
+
+const AuthContext = useContext(null);
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    //Cek ke Local Storage ada apa kagak UserInfo?
+    useEffect(()=> {
+        try {
+            const userInfo = localStorage.getItem('userInfo');
+            if(userInfo) {
+                const parsedInfo = JSON.parse(userInfo);
+                setUser(parsedInfo);
+                apiClient.defaults.headers.common['Authorization'] = `Bearer ${parsedInfo.token}`
+            }
+        } catch (error) {
+            console.error('Gagal Mengambil data User Dari Local Storage', error);
+            localStorage.removeItem('userInfo');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    // Login Logika nye
+    const login = async(email, password) => {
+        const {data} = await apiClient.post('/users/login', {email, password});
+        if(data) {
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            setUser(data);
+            apiClient.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+        }
+        return data;
+    };
+
+    // Logika Register uy
+    const register = async(name, email, password) => {
+        const {data} = await apiClient.post('/users/register', {name, email, password});
+        if(data) {
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            setUser(data);
+            apiClient.defaults.headers.common['Authorization'] = `Bearer ${data.token}`;
+        }
+        return data;
+    };
+
+    // Logika Buat Logout
+    const logout = () => {
+        localStorage.removeItem('userInfo');
+        setUser(null);
+        delete apiClient.defaults.headers.common['Authorization'];
+    };
+
+    const value = {
+        user,
+        isAuthenticated: !!user,
+        loading,
+        login,
+        register,
+        logout,
+    };
+
+    return(
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+
+};
+
+export const useAuth = () => {
+    return(useContext(AuthContext))
+};
--- Ngopi-Frontend/src/main.jsx
+++ Ngopi-Frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './routes/index.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter />
+    <AuthProvider >
+      <AppRouter />
+    </AuthProvider>
   </React.StrictMode>,
 )
