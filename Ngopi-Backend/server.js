92d76353036715244fbacc4c36b512d270b7a53f
RetiortUK <aauliagazzam@gmail.com>
2025-10-07 10:59:51 +0700

    Fix: URL FRONTEND

--- Ngopi-Backend/server.js
+++ Ngopi-Backend/server.js
@@ -22,13 +22,13 @@ const app = express();
 
 const httpServer = http.createServer(app);
 
-// TODO: FIX LOCALHOST TO VERCELAPP FRONTEND
 const io = new Server(httpServer, {
     cors: {
         origin: process.env.FRONTEND_URL || "http://localhost:5173",
         methods: ["GET", "POST"]
     }
 });
+
 
 app.use((req, res, next) => {
     req.io = io;
